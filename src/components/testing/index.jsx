87d0592e650f3810a37a448129116b9aff18ed14
codermdshakil <ahmedshakil0512@gmail.com>
2024-01-25 21:42:37 +0600

    shakil ahmed make some changes on testing folder

--- src/components/testing/index.jsx
+++ src/components/testing/index.jsx
@@ -2,6 +2,7 @@ const Test = () => {
 
     return <div>
         <h2>Testing  noyon vai er project</h2>
+        <p>Project Description. Lorem ipsum, dolor sit amet consectetur adipisicing elit. Error ex laboriosam consequuntur nesciunt itaque adipisci nihil sint rerum facilis blanditiis.</p>
     </div>
 }
 
